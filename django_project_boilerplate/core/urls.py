83d609fa28f79d4169818237e7d06381ac0005ea
abdullah <abdullah-s-guser@hotmail.com>
2021-05-09 22:18:37 +0300

    Changed navigation bar

--- django_project_boilerplate/core/urls.py
+++ django_project_boilerplate/core/urls.py
@@ -5,5 +5,7 @@ from .views import home, products, checkout
 app_name='core'
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('checkout/', checkout, name='checkout'),
+    path('products/', products, name='products'),
 ]
--- django_project_boilerplate/core/views.py
+++ django_project_boilerplate/core/views.py
@@ -2,13 +2,6 @@ from django.shortcuts import render
 from .models import Item
 
 
-def item_list(request):
-    context = {
-        'items': Item.objects.all()
-    }
-    return render(request, "home-page.html", context)
-
-
 def products(request):
     context = {
         'items': Item.objects.all()
