a3c71378edbe08bc9791c854cabd5177d6c09bfd
abdullah <abdullah-s-guser@hotmail.com>
2021-05-09 23:55:56 +0300

    Added class based views for home and product pages

--- django_project_boilerplate/core/urls.py
+++ django_project_boilerplate/core/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from .views import home, products, checkout
+from .views import (
+    HomeView, 
+    ItemDetailView, 
+    checkout
+)
 
 
 app_name='core'
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', HomeView.as_view(), name='home'),
     path('checkout/', checkout, name='checkout'),
-    path('products/', products, name='products'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
 ]
